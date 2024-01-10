e3c767875a845a9da746432b78534a96d87d43fc
Yasmin Ibrahim <yasmin@Yasmins-MacBook-Air.local>
2024-01-10 14:01:31 +0000

    added new button to index but can't see it in the browser yet. Attempts to add text to local storage currently not working

--- assets/js/day.js
+++ assets/js/day.js
@@ -1,14 +1,50 @@
+// nineTextEl = $('#nineText');
+// tenTextEl = $('#tenText');
+// elevenTextEl = $('#elevenText');
+// twelveTextEl = $('#twelveText');
+// thirteenTextEl = $('#thirteenText');
+// fourteenTextEl = $('#fourteenText');
+// fifteenTextEl = $('#fifteenText');
+// sixteenTextEl = $('#sixteenText');
+// seventeenTextEl = $('#seventeenText');
+
+// // Array of query selectors
+// const textAreas = [
+//    nineTextEl, 
+//     tenTextEl, 
+//     elevenTextEl,
+//     twelveTextEl,
+//     thirteenTextEl,
+//     fourteenTextEl,
+//     fifteenTextEl,
+//     sixteenTextEl,
+//     seventeenTextEl
+// ];
+
+// Array of query selectors
+const textAreas = [
+    $('#nineText'), 
+    $('#tenText'), 
+    $('#elevenText'),
+    $('#twelveText'),
+    $('#thirteenText'),
+    $('#fourteenText'),
+    $('#fifteenText'),
+    $('#sixteenText'),
+    $('#seventeenText')
+];
+
 $(document).ready(function () {
 
     function displayTime() {
-        $('#currentDay').text(dayjs().format('dddd, MMMM D, YYYY'))
+        $('#currentDate').text(dayjs().format('ddd MMM D, YYYY'))
     }
 
     setInterval(displayTime, 1000)
 
     function updateHour() {
         let currentHour = dayjs().hour()
-        let timeBlocks = $('.time-block')
+        let timeBlocksEl = $('.time-block')
 
         timeBlocks.each(function () {
             let blockHour = parseInt($(this).attr('id'))
@@ -30,13 +66,31 @@ $(document).ready(function () {
             setInterval(updateHour, 15000)
 
 
-            $('saveBtn').click(function () {
-                // grab values of the textareas (c;ass pf description)
-                // save the values to local storage using the ids as the keys and the values of the textareas for the values
-                // 
-            })
+            $('saveBtn').click(function (e) {
+                e.preventDefault();
+            // Loop through each query selector???
+            $.each(textAreas, function(i, selector) {
 
-            // load any saced data from localStorages 
+                if (textAreas[i] !== '') {
+                // grab the element using the current query selector
+                const currentTextEl = $(selector);
+            
+                // grab the text content of the element
+                const textContent = currentTextEl.text();
+            
+                // Save the text content to local storage with a key based on the index
+                localStorage.setItem(`textElement${i + 1}`, textContent);
+                
+                console.log(textContent);
+            }
+        else {
+            console.log("nothing logged");
+        }
+    });
+            console.log('Loop completed.');
+            console.log(e);
+            })
+            // load any saved data from localStorages 
         })
 
 
@@ -44,3 +98,14 @@ $(document).ready(function () {
     }
 })
 
+// OLD PSEUDOCODE/NOTES FOR STORING TO LOCAL
+// $('saveBtn').click(function (event) {
+//     event.preventDefault();
+//     // validate the fields?
+//     // grab values of the textareas (class of the text description)
+//     // save the values to local storage using the ids as the keys and the values of the textareas for the values
+//     // 
+
+ 
+// // load any saved data from localStorages    
+// })
